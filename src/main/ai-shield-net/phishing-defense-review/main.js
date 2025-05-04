ad326622601465ffde1eef84a14cba56d9676d1f
Trong Hieu <info@prosfinity.com>
2025-05-04 22:00:33 +0700

    feat: update

--- src/main/ai-shield-net/phishing-defense-review/main.js
+++ src/main/ai-shield-net/phishing-defense-review/main.js
@@ -24,124 +24,6 @@ const PhishingDefenseReviewMain = () => {
             : ""
         } text-center text-3xl md:text-3xl font-bold text-gray-800 leading-tight md:leading-tight mt-5`}
       >
-        Private Phishing Technical Advisor Session
-      </h2>
-      <p
-        className={`${
-          loaded || inViewport
-            ? `animate__animated animate__slideInUp animate__fast`
-            : ""
-        } text-lg md:text-lg leading-tight md:leading-normal mt-5`}
-      >
-        Get a personalized phishing defense assessment with our expert advisor.
-        In this private session, we’ll review your current endpoint protection
-        strategy, simulate common phishing attack scenarios, and provide
-        actionable insights on how to improve your defenses against phishing
-        threats.
-      </p>
-      <p
-        className={`${
-          loaded || inViewport
-            ? `animate__animated animate__slideInUp animate__fast`
-            : ""
-        } text-sm md:text-base leading-tight md:leading-normal mt-5`}
-      >
-        Exclusive for 10 organizations: 30-day free trial + only HKD $9.99/month
-        thereafter.
-      </p>
-      <div className="flex justify-center items-center my-5">
-        <div
-          className={`${
-            loaded || inViewport
-              ? `animate__animated animate__slideInUp animate__fast`
-              : ""
-          } shadow-xl rounded-lg`}
-        >
-          <img
-            src="/images/phishing-defense-review/main.png"
-            alt="phishing-defense-review"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-2 mt-5">
-        <div
-          className={`${
-            loaded || inViewport
-              ? `animate__animated animate__bounceInLeft animate__fast`
-              : ""
-          } flex items-center gap-2`}
-        >
-          <CheckIcon />
-          <p className="text-sm xl:text-base text-black">
-            Get ZDR free for the first month
-          </p>
-        </div>
-        <div
-          className={`${
-            loaded || inViewport
-              ? `animate__animated animate__bounceInLeft animate__fast`
-              : ""
-          } flex items-center gap-2`}
-        >
-          <SirenIcon />
-          <p className="text-sm xl:text-base text-black">
-            1-on-1 consultation with a cybersecurity expert
-          </p>
-        </div>
-        <div
-          className={`${
-            loaded || inViewport
-              ? `animate__animated animate__bounceInLeft animate__fast`
-              : ""
-          } flex items-center gap-2`}
-        >
-          <BanknoteIcon />
-          <p className="text-sm xl:text-base text-black">
-            Only HKD $9.99/month thereafter
-          </p>
-        </div>
-        <div
-          className={`${
-            loaded || inViewport
-              ? `animate__animated animate__bounceInLeft animate__fast`
-              : ""
-          } flex items-center gap-2`}
-        >
-          <InfinityIcon />
-          <p className="text-sm xl:text-base text-black">
-            Limited to the first 10 organizations
-          </p>
-        </div>
-      </div>
-      <h2
-        className={`${
-          loaded || inViewport
-            ? `animate__animated animate__slideInUp animate__fast`
-            : ""
-        } text-center text-3xl md:text-3xl font-bold text-gray-800 leading-tight md:leading-tight mt-10`}
-      >
-        AI ShieldNet Feature Demo Block Zero-day Phishing and Ransomware
-      </h2>
-      <div className="mt-10 w-full md:w-3/4 mx-auto">
-        <iframe
-          width="100%"
-          height="600"
-          src="https://www.youtube.com/embed/jDOWVF6bpxw?si=x94cOKZ3tqErLAFc"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <h2
-        className={`${
-          loaded || inViewport
-            ? `animate__animated animate__slideInUp animate__fast`
-            : ""
-        } text-center text-3xl md:text-3xl font-bold text-gray-800 leading-tight md:leading-tight mt-10`}
-      >
         Schedule a Phishing Defense Review
       </h2>
       <div className="flex flex-col md:flex-row gap-2 mt-5">
@@ -303,6 +185,124 @@ const PhishingDefenseReviewMain = () => {
             </button>
           </form>
         </div>
+      </div>
+      <h2
+        className={`${
+          loaded || inViewport
+            ? `animate__animated animate__slideInUp animate__fast`
+            : ""
+        } text-center text-3xl md:text-3xl font-bold text-gray-800 leading-tight md:leading-tight mt-10`}
+      >
+        Private Phishing Technical Advisor Session
+      </h2>
+      <p
+        className={`${
+          loaded || inViewport
+            ? `animate__animated animate__slideInUp animate__fast`
+            : ""
+        } text-lg md:text-lg leading-tight md:leading-normal mt-5`}
+      >
+        Get a personalized phishing defense assessment with our expert advisor.
+        In this private session, we’ll review your current endpoint protection
+        strategy, simulate common phishing attack scenarios, and provide
+        actionable insights on how to improve your defenses against phishing
+        threats.
+      </p>
+      <p
+        className={`${
+          loaded || inViewport
+            ? `animate__animated animate__slideInUp animate__fast`
+            : ""
+        } text-sm md:text-base leading-tight md:leading-normal mt-5`}
+      >
+        Exclusive for 10 organizations: 30-day free trial + only HKD $9.99/month
+        thereafter.
+      </p>
+      <div className="flex justify-center items-center my-5">
+        <div
+          className={`${
+            loaded || inViewport
+              ? `animate__animated animate__slideInUp animate__fast`
+              : ""
+          } shadow-xl rounded-lg`}
+        >
+          <img
+            src="/images/phishing-defense-review/main.png"
+            alt="phishing-defense-review"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 mt-5">
+        <div
+          className={`${
+            loaded || inViewport
+              ? `animate__animated animate__bounceInLeft animate__fast`
+              : ""
+          } flex items-center gap-2`}
+        >
+          <CheckIcon />
+          <p className="text-sm xl:text-base text-black">
+            Get ZDR free for the first month
+          </p>
+        </div>
+        <div
+          className={`${
+            loaded || inViewport
+              ? `animate__animated animate__bounceInLeft animate__fast`
+              : ""
+          } flex items-center gap-2`}
+        >
+          <SirenIcon />
+          <p className="text-sm xl:text-base text-black">
+            1-on-1 consultation with a cybersecurity expert
+          </p>
+        </div>
+        <div
+          className={`${
+            loaded || inViewport
+              ? `animate__animated animate__bounceInLeft animate__fast`
+              : ""
+          } flex items-center gap-2`}
+        >
+          <BanknoteIcon />
+          <p className="text-sm xl:text-base text-black">
+            Only HKD $9.99/month thereafter
+          </p>
+        </div>
+        <div
+          className={`${
+            loaded || inViewport
+              ? `animate__animated animate__bounceInLeft animate__fast`
+              : ""
+          } flex items-center gap-2`}
+        >
+          <InfinityIcon />
+          <p className="text-sm xl:text-base text-black">
+            Limited to the first 10 organizations
+          </p>
+        </div>
+      </div>
+      <h2
+        className={`${
+          loaded || inViewport
+            ? `animate__animated animate__slideInUp animate__fast`
+            : ""
+        } text-center text-3xl md:text-3xl font-bold text-gray-800 leading-tight md:leading-tight mt-10`}
+      >
+        AI ShieldNet Feature Demo Block Zero-day Phishing and Ransomware
+      </h2>
+      <div className="mt-10 w-full md:w-3/4 mx-auto">
+        <iframe
+          width="100%"
+          height="600"
+          src="https://www.youtube.com/embed/jDOWVF6bpxw?si=x94cOKZ3tqErLAFc"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
