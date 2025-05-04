d77736ec019db5b3a7cb564971e479137320cf8b
Trong Hieu <info@prosfinity.com>
2025-05-04 22:34:39 +0700

    feat: google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,21 @@ export default function Document() {
         <Analytics />
         <NextScript />
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QQE488VPHG"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QQE488VPHG');
+            `,
+          }}
+        />
       </body>
     </Html>
   );
