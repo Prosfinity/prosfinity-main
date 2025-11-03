79fe248f5aa8f3c717e2959cea9818c464c21540
Trong Hieu <tronghieu60s@gmail.com>
2025-11-03 10:40:18 +0700

    feat: new version 1.1.0

--- src/pages/product/AIShieldNet/download-and-price/agent-download/index.js
+++ src/pages/product/AIShieldNet/download-and-price/agent-download/index.js
@@ -102,9 +102,9 @@ const AIShieldNetDownloadAndPriceAgentDownloadPage = () => {
                     <td className="px-6 py-4 text-gray-900 ">
                       Execute File (.exe)
                     </td>
-                    <td className="px-6 py-4 text-gray-900 ">9410 KB</td>
+                    <td className="px-6 py-4 text-gray-900 ">18.1 MB</td>
                     <td className="px-6 py-4">
-                      <a href="/files/AIShieldNet_agent_1.0.9.exe" target="_blank">
+                      <a href="/files/AIShieldNet_agent_1.1.0.exe" target="_blank">
                         <button className="bg-btnBlue rounded-lg px-4 py-2 text-sm font-medium text-white transition-all hover:shadow-xl md:px-8 md:py-2 md:text-sm">
                           Download Now
                         </button>
