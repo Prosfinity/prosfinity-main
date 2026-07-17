a444bdc34400323fd1753c1d67ecfd1ac9a4c128
ClawForge <clawforge@ClawForgedeMac-mini.local>
2026-07-17 14:25:27 +0800

    Add visual BDR developer installation guide

--- src/main/home/navbar-new.js
+++ src/main/home/navbar-new.js
@@ -98,6 +98,11 @@ const bdrItems = [
     "Plan a low-friction pilot and progressive rollout.",
     "/product/browser-detection-response/deployment",
   ],
+  [
+    "Developer Installation",
+    "Build, install and verify the managed Chrome extension.",
+    "/product/browser-detection-response/developer-installation",
+  ],
 ];
 
 const NavbarNew = () => {
